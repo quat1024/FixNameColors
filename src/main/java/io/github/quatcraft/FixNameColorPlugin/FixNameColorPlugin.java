076a80d51977cdf3d6f4e94b0fa1a952c5babf4d
quat <scratch933@gmail.com>
2016-06-05 21:04:23 -0400

    "Fixes" #2
    im the best

--- src/main/java/io/github/quatcraft/FixNameColorPlugin/FixNameColorPlugin.java
+++ src/main/java/io/github/quatcraft/FixNameColorPlugin/FixNameColorPlugin.java
@@ -1,9 +1,5 @@
 package io.github.quatcraft.FixNameColorPlugin;
 
-/*
- * Created by quat on 5/13/2016.
- */
-
 
 import org.bukkit.Server;
 import org.bukkit.event.*;
@@ -30,11 +26,15 @@ public class FixNameColorPlugin extends JavaPlugin implements Listener {
 
     @EventHandler(priority = EventPriority.LOW)
     public void onPlayerChat(AsyncPlayerChatEvent e) {
-        if(e.getPlayer() != null) {
-            Team team = e.getPlayer().getScoreboard().getPlayerTeam(e.getPlayer());
-            String prefix = team.getPrefix();
-            String suffix = team.getSuffix();
-            e.setFormat("<"+ prefix + "%1$s" + suffix + "> %2$s");
+        try {
+            if (e.getPlayer() != null) {
+                Team team = e.getPlayer().getScoreboard().getPlayerTeam(e.getPlayer());
+                String prefix = team.getPrefix();
+                String suffix = team.getSuffix();
+                e.setFormat("<" + prefix + "%1$s" + suffix + "> %2$s");
+            }
+        } catch (Exception iGuessTheyreNotOnATeam) {
+            ; //I'm a good programmer!
         }
     }
 }
